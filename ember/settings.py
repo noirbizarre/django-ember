7645c5f49816223baa34f0bd4f1a1dd0b2f8d1c8
Axel Haustant <noirbizarre@gmail.com>
2012-10-09 10:00:29 +0200

    Updated dependencies

--- ember/settings.py
+++ ember/settings.py
@@ -118,7 +118,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'ember'
+    'djangojs',
+    'ember',
 )
 
 # A sample logging configuration. The only tangible logging
